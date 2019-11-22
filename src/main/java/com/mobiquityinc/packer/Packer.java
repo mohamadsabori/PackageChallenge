6ae0cc86c49a4ed243cc4055d4152f994b3568cb
mohamadsabori <m.sabori.86@gmail.com>
2019-11-22 14:30:30 +0330

    s

--- src/main/java/com/mobiquityinc/packer/Packer.java
+++ src/main/java/com/mobiquityinc/packer/Packer.java
@@ -1,12 +1,12 @@
 package main.java.com.mobiquityinc.packer;
 
 import main.java.com.mobiquityinc.exception.APIException;
+
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CodingErrorAction;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Packer {
 
@@ -29,16 +29,18 @@ public class Packer {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        InputStreamReader reader = new InputStreamReader(input,decoder);
+        InputStreamReader reader = new InputStreamReader(input, decoder);
         try (BufferedReader br = new BufferedReader(reader)) {
             String line = null;
             StringBuffer allLinesResult = new StringBuffer();
             while ((line = br.readLine()) != null) {
+                SortedMap<Float, List> finalValues = new TreeMap<>();
                 String[] splitedLines = line.split(" : ");
                 float weightOfPackage = Float.parseFloat(splitedLines[0]);
-                Map<Integer,Float> result = prepareAllLines(splitedLines[1], weightOfPackage);
-                float maxPackagePossible = findPossiblePackages(result);
-                allLinesResult.append(prepareOutPut(result, maxPackagePossible)).append("\n");
+                finalValues = prepareAllLines(splitedLines[1], weightOfPackage);
+//                float maxPackagePossible = findPossiblePackages(result);
+//                allLinesResult.append(prepareOutPut(result, maxPackagePossible)).append("\n");
+                allLinesResult.append(prepareOutPut(finalValues)).append("\n");
             }
             return allLinesResult.toString();
         } catch (IOException e) {
@@ -46,16 +48,36 @@ public class Packer {
         }
     }
 
-    private static Map<Integer, Float> prepareAllLines(String splitedLine, float weightOfPackage) {
-        Map<Integer, Float> result = new HashMap<>();
+    private static String prepareOutPut(SortedMap<Float, List> finalValues) {
+        if(finalValues.isEmpty()){
+            return "-";
+        }
+        List<Integer> list = finalValues.get(finalValues.lastKey());
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < list.size(); i++) {
+            buffer.append(list.get(i)).append(i == list.size() - 1 ? "" : ",");
+        }
+        return buffer.toString();
+    }
+
+    private static void sortResultByCost(Map<Integer, Float> result) {
+
+    }
+
+    private static SortedMap<Float, List> prepareAllLines(String splitedLine, float weightOfPackage) {
+        SortedMap<Float, List> finalValues = new TreeMap<>();
         for (String s : splitedLine.split(" ")) {
             String[] packageValues = s.split(",");
-            if(Float.parseFloat(packageValues[1]) < weightOfPackage){
-                result.put( Integer.parseInt(packageValues[0].substring(1))
-                        , Float.valueOf(packageValues[2].substring(0,packageValues[2].indexOf(')'))));
+            if (Float.parseFloat(packageValues[1]) < weightOfPackage) {
+                Float costOfPackage = Float.valueOf(packageValues[2].substring(0, packageValues[2].indexOf(')')));
+                int idOfPackage = Integer.parseInt(packageValues[0].substring(1));
+                if(!finalValues.containsKey(costOfPackage)){
+                    finalValues.put(costOfPackage, new ArrayList());
+                }
+                finalValues.get(costOfPackage).add(idOfPackage);
             }
         }
-        return result;
+        return finalValues;
     }
 
     private static float findPossiblePackages(Map<Integer, Float> result) {
@@ -68,15 +90,15 @@ public class Packer {
 
     private static String prepareOutPut(Map<Integer, Float> result, float maxPackagePossible) {
         StringBuffer buffer = new StringBuffer();
-        if(result.isEmpty()){
+        if (result.isEmpty()) {
             buffer.append("-");
-        }else{
+        } else {
             for (Map.Entry<Integer, Float> resultValue : result.entrySet()) {
-                if( resultValue.getValue() == maxPackagePossible){
+                if (resultValue.getValue() == maxPackagePossible) {
                     buffer.append(resultValue.getKey()).append(",");
                 }
             }
-            buffer.replace(buffer.lastIndexOf(","),buffer.length(),"");
+            buffer.replace(buffer.lastIndexOf(","), buffer.length(), "");
         }
         return buffer.toString();
     }
